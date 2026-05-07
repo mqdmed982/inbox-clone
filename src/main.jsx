1fab11a6c463340376d28ae8cbb3c2ac174cff3e
mqdmed982 <mqdmed982@gmail.com>
2026-05-07 14:55:15 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDom from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css'  // <--- HAD L-STR DARORI!
 
-ReactDom.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
